77726993988c17e8a05a32eed25d04ff3557e4fe
sshamsSVA <sshams@svaprogram.org>
2022-10-21 08:19:52 -0400

    Part 4 completed

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class RubyController : MonoBehaviour
 {
+    public float horizontal;
+    public float vertical;
+    float speed = 40;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +16,13 @@ public class RubyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        // Declared postion variable and set it equal to player's position
         Vector2 position = transform.position;
-        position.x = position.x + 0.1f; 
+        // Update player's x position .1 unit every frame
+        position.x = position.x + 0.1f * horizontal * Time.deltaTime * speed; 
+        position.y = position.y + 0.1f * vertical * Time.deltaTime * speed;
         transform.position = position;
     }
 }
